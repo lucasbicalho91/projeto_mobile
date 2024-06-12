0f6f1c0b5abeb0fc645fbce03d1d53bc24039e29
Lucas Bicalho <bicalho.lucas@gmail.com>
2024-06-12 20:51:49 -0300

    Configurando a estrutura

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,9 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
+
+import { CreateUserController } from './controllers/User/CreateUserController';
 
 const router = Router();
 
-router.get('/teste', (req: Request, res: Response) => {
-  return res.json({ nome: 'Pizzaria TADS' })
-})
+router.post('/users', new CreateUserController().handle)
 
 export { router };
