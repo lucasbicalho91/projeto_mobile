36db1fd32c58b7ad6d65e56aa525e6e6fd9685b8
Lucas Bicalho <bicalho.lucas@gmail.com>
2024-06-12 21:48:34 -0300

    criado o token de autenticação

--- backend/src/services/user/AuthUserService.ts
+++ backend/src/services/user/AuthUserService.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../../prisma";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 interface AuthRequest {
   email: string,
@@ -25,9 +26,24 @@ class AuthUserService {
       throw new Error("Usuário ou senha incorreto");
     }
 
+    const token = sign(
+      {
+        name: user.name,
+        email: user.email
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: '30d'
+      }
+    ) 
 
-
-    return { ok: true }
+    return { 
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      token: token
+     }
   }
 }
 
