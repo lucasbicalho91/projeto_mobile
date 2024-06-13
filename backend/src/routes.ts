d05f455c3d7e8b63cbf99ff358695aa01c403270
Lucas Bicalho <bicalho.lucas@gmail.com>
2024-06-12 21:32:05 -0300

    criptografando a senha

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import { CreateUserController } from './controllers/User/CreateUserController';
+import { AuthUserController } from './controllers/User/AuthUserController';
 
 const router = Router();
 
-router.post('/users', new CreateUserController().handle)
+router.post('/users', new CreateUserController().handle);
+router.post('/session', new AuthUserController().handle);
 
 export { router };
