5ee2a8faa40056a8ea0276a7340626a92a29e132
Lucas Bicalho <bicalho.lucas@gmail.com>
2024-06-15 10:35:59 -0300

    adicionando e removendo itens de um pedido

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -13,6 +13,9 @@ import { ListByCategoryController } from './controllers/product/ListByCategoryCo
 import { CreateOrderController } from './controllers/order/CreateOrderController';
 import { RemoveOrderController } from './controllers/order/RemoveOrderController';
 
+import { AddItemController } from './controllers/order/AddItemController';
+import { RemoveItemController } from './controllers/order/RemoveItemController';
+
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
 const router = Router();
@@ -33,5 +36,7 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 //Rotas de Pedido
 router.post('/order', isAuthenticated, new CreateOrderController().handle);
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle);
+router.post('/order/add', isAuthenticated, new AddItemController().handle);
+router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle);
 
 export { router };
